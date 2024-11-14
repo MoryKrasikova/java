1bea51b557f473bd6caf852834d4e47a7aabbb8b
Мария <masha121105@gmail.com>
2024-11-14 22:35:41 +0700

    добавление статического поля и метода

--- src/GameOutcome.java
+++ src/GameOutcome.java
@@ -3,22 +3,26 @@
 public class GameOutcome {
     private int rightanswers;
     private int wronganswers;
-
+    static int kolgame;
     public GameOutcome(int rightAnswers, int wrongAnswers) {
         this.rightanswers = rightAnswers;
         this.wronganswers = wrongAnswers;
+        kolgame += 1;
     }
 
-    public int getRightAnswers() {
+    public int GetRightAnswers() {
         return rightanswers;
     }
 
-    public int getWrongAnswers() {
+    public int GetWrongAnswers() {
         return wronganswers;
     }
 
     @Override
     public String toString() {
         return "Правильные ответы: " + rightanswers + ", Неверные ответы: " + wronganswers;
+    }
+    public static int GetKolGame() {
+        return kolgame;
     }
 }
